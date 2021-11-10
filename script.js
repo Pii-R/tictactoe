60aa18f2900ad601f57662a42f9a422a8533d170
phenry <pierre.henry51@gmail.com>
2021-11-11 00:24:10 +0100

    img + changes

--- script.js
+++ script.js
@@ -6,77 +6,66 @@ let b_reset = document.getElementById('button3')
 let choices_array  = ["x","o",""]
 let choice = null
 let count_click = 0 //it will follow how many clicks on button there are
-let x_button_click = 0
-let o_button_click = 0
-let first_choice
 let win = false
-document.addEventListener('click',change_text, false);
-document.addEventListener('click',check_win,false)
 let grid = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+let dict_img_to_sym = {"images/circle.png":"o","images/cross.png":"x"}
+//eventListener
+document.addEventListener('click',change_text, false);
+document.addEventListener('click',update_grid,false)
+
 
 function check_array(arr){
     s = new Set(arr)
     if (s.size == 1 & (s.has("x") | s.has("o"))){
+        alert(s.values().next().value+ " has win")
         return true
     }
     return false
 }
 
-function check_win(){
-    for(i=0;i<9;i++){
-        if (document.getElementById(i).innerHTML != null & document.getElementById(i).innerHTML != "" & (grid[i] != "x" | grid[i] != "o")){ 
-        grid[i]=document.getElementById(i).innerHTML
-        }
-    }
-    console.log(grid)
+function update_grid(){
+    // update grid when player click on the board
+
     // over line
     if (!win){
-    s_diag_1 = new Set([grid[0],grid[4],grid[8]])
-    s_diag_2 = new Set([grid[2],grid[4],grid[6]])
-
-    if (check_array(s_diag_1)){
-        alert("win over first diag")
-        win = true
-        return
-
-    }
-    else if (check_array(s_diag_2)) {
-        alert("win over second diag")
-        win = true
-        return
-    }
-
+        s_diag_1 = new Set([grid[0],grid[4],grid[8]])
+        s_diag_2 = new Set([grid[2],grid[4],grid[6]])
     
-    for (let i=0;i<3;i++){
-        s_line = new Set([grid[0+i*3],grid[i*3+1],grid[i*3+2]])
-        s_col = new Set([grid[0+i],grid[3+i],grid[6+i]])
-        console.log(s_line)
-        if (check_array(s_line)){
-            alert("win over line")
+        if (check_array(s_diag_1)){
+            alert("win over first diag")
             win = true
             return
-            
+
         }
-        if (check_array(s_col)){
-            alert("win over colums")
+        else if (check_array(s_diag_2)) {
+            alert("win over second diag")
             win = true
             return
-            
-        }       
         }
-
+        for (let i=0;i<3;i++){
+            s_line = new Set([grid[0+i*3],grid[i*3+1],grid[i*3+2]])
+            s_col = new Set([grid[0+i],grid[3+i],grid[6+i]])
+            if (check_array(s_line)){
+                alert("win over line")
+                win = true
+                return
+            }
+            if (check_array(s_col)){
+                alert("win over colums")
+                win = true
+                return
+            }       
+        }
     }
-    }
-    
-
+}
 
 function get_next_choice(choice){
     switch (choice){
-        case "x":
-            next_choice = "o"
+        case "images/cross.png":
+            next_choice = "images/circle.png"
             return next_choice
-        case "o":
-            next_choice = "x" 
+        case "images/circle.png":
+            next_choice = "images/cross.png" 
             return next_choice
     }
 }
@@ -84,45 +73,43 @@ function get_next_choice(choice){
 function change_text(e) {
     var tgt= e.target
     if (choice !== null){
-    if (tgt.innerHTML == "" & tgt.parentElement.className=='grid-container' &
-    tgt.className!='grid-container'){  
-    tgt.innerHTML =  choice
-    choice = get_next_choice(choice) 
-    count_click++
-    console.log("next_choice is " + choice,count_click)
-    }
-    else if (!choices_array.includes(tgt.innerHTML) & tgt.parentElement.className=='grid-container') {
-     tgt.innerHTML="";
+        if (tgt.parentElement.className=='grid-container' &
+            tgt.className!='grid-container' & 1){
+            tgt.children[0].src = choice
+            tgt.children[0].style.display="block"
+            index = parseInt(tgt.id)
+            grid[index]=dict_img_to_sym[choice]
+            console.log(grid)
+            // tgt.innerHTML =  choice
+            choice = get_next_choice(choice) 
+            count_click++
+            console.log("next_choice is " + choice,count_click)
+            }
+        else if (!choices_array.includes(tgt.innerHTML) & tgt.parentElement.className=='grid-container') {
+            tgt.innerHTML="";
+        }
     }
 }
-    
-    else {
-        console.log('choice not defined')
-    }
 
-}
-
-b1.onclick = function(){
+b1.onclick = function(){ // button to choose cross
     if (count_click == 0){
-    choice ="x";
+    choice ="images/cross.png";
     }
 }
 
-b2.onclick = function(){
+b2.onclick = function(){// button to choose circle
     if (count_click == 0){
-    choice ="o";  
+    choice ="images/circle.png";  
     }
 }
    
-
 b_reset.onclick = function(){
     for (i=0;i<9;i++){
-        document.getElementById(i).innerHTML = "";
+        document.getElementById(i).children[0].src = "";
     }
     win = false
     count_click = 0
     choice = null
     grid = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
 }
-
 };
